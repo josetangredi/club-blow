b14802bf73976be834aad5499347d9a57cabd16a
josetangredi <tangredijosefina@gmail.com>
2024-10-22 10:42:59 -0300

    cambios blow

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-// Seleccionamos el botón de scroll
-const scrollButton = document.getElementById('scrollButton');
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
-// Inicializamos un contador para rastrear la sección actual
-let currentSection = 0;
-
-// Array de IDs de secciones en el orden para el scroll hacia abajo
-const sections = ['intro', 'nosotros', 'socios', 'reprocann'];
-
-// Añadimos un evento de clic al botón
-scrollButton.addEventListener('click', function () {
-    // Si hay más secciones para desplazarse
-    if (currentSection < sections.length) {
-        // Hacemos scroll suave hacia la sección actual
-        document.getElementById(sections[currentSection]).scrollIntoView({ behavior: 'smooth' });
-        // Aumentamos el contador para la próxima sección
-        currentSection++;
-    } else {
-        // Si hemos llegado al final, volvemos a la sección de inicio y reiniciamos el contador
-        document.getElementById('inicio').scrollIntoView({ behavior: 'smooth' });
-        currentSection = 0; // Reiniciamos el contador
-    }
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
